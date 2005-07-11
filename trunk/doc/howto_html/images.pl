c770348be1ba984584e99a5de04ca8c4672bcd79
lgaballo <lgaballo@ae816e9b-97f9-0310-aa70-bf55b011afa0>
2005-07-11 08:37:24 +0000

    Added doc

---
+++ trunk/doc/howto_html/images.pl
@@ -0,0 +1,103 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate images original text with physical files.
+
+
+$key = q/{lstlisting}[frame=trbl,caption={Startdiscovercode},label=discover]{}deferr=defes(stunPort,reactor)d.addCallback(succeed)d.addErrback(fail){par{{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="162" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\begin{lstlisting}[frame=trbl,caption={Start discover code},label=discover]{}
+de...
+...unPort, reactor)
+d.addCallback(succeed)
+d.addErrback(fail)
+\par
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[frame=trbl,caption={StartTCPconnection},label=thread]{}#StarttolistpeerPort,EchoClientFactory(),30,(myIP,myPort))reactor.run(){par{{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="258" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="\begin{lstlisting}[frame=trbl,caption={Start TCP connection},label=thread]{}
+...">|; 
+
+$key = q/{lstlisting}[frame=trbl,caption={Failurecallbackfunctioncode},label=fail]{}deffail(failure):"""FailinSTUNdiscover"""printfailure{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="86" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\begin{lstlisting}[frame=trbl,caption={Failure callback function code},label=fai...
+...l(failure):
+'''''' Fail in STUN discover ''''''
+print failure
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[frame=trbl,caption={test.conf},label=test]{}[CB]#ConnectionBrokerCB109myPort:50007{par{[UDPhole]#FortheUDPcommunicationudpPort:9999{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="372" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="\begin{lstlisting}[frame=trbl,caption={test.conf},label=test]{}
+[CB]
+...">|; 
+
+$key = q/{lstlisting}[frame=trbl,caption={Succeededcallbackfunctioncode},label=succeed]{}etconf,id)d.addCallback(registrationMade)d.addErrback(fail){par{{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="430" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="\begin{lstlisting}[frame=trbl,caption={Succeeded callback function code},label=s...
+..., id)
+d.addCallback(registrationMade)
+d.addErrback(fail)
+\par
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[frame=trbl,caption={Importpackets},label=import]{}importp2pNetwork.2pNetwork.htcp.puncheraspunchimporttwisted.internet.deferasdefer{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="86" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\begin{lstlisting}[frame=trbl,caption={Import packets},label=import]{}
+import p2...
+...rk.htcp.puncher as punch
+import twisted.internet.defer as defer
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[frame=trbl,caption={Importpackets},label=importTCP]{}importConfigPa.sniffyassnifferimportp2pNetwork.testTCP.udpSnifferasudp_sniffer{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="86" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="\begin{lstlisting}[frame=trbl,caption={Import packets},label=importTCP]{}
+import...
+... as sniffer
+import p2pNetwork.testTCP.udpSniffer as udp_sniffer
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[frame=trbl,caption={p2pNetwork.conf},label=p2pNetwork]{}[stun]#STUNpeerconfiguration[holePunch]ConnectionBroker:10.193.163.246:6060{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="277" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="\begin{lstlisting}[frame=trbl,caption={p2pNetwork.conf},label=p2pNetwork]{}
+[stu...
+...configuration
+[holePunch]
+ConnectionBroker: 10.193.163.246:6060
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[frame=trbl,caption={Loadconfiguration},label=config]{}#Loadconfigur.get('peer','peerIP')peerPort=int(config.get('peer','peerPort')){lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="181" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="\begin{lstlisting}[frame=trbl,caption={Load configuration},label=config]{}
+...">|; 
+
+$key = q/{lstlisting}[frame=trbl,caption={CallbackfunctioncodeafterCBregistration},label=onnectwiththeotherpeer"""{par{puncher.connectByURI(peerURI){par{{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="162" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="\begin{lstlisting}[frame=trbl,caption={Callback function code after CB registrat...
+...h the other peer ''''''
+\par
+puncher.connectByURI(peerURI)
+\par
+\end{lstlisting}">|; 
+
+1;
+
---
+++ trunk/doc/howto_html/labels.pl
@@ -0,0 +1,53 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate labels original text with physical files.
+
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.70)
+# labels from external_latex_labels array.
+
+
+$key = q/test/;
+$external_latex_labels{$key} = q|10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fail/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/discover/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/succeed/;
+$external_latex_labels{$key} = q|4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/import/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/dteg3/;
+$external_latex_labels{$key} = q|5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/thread/;
+$external_latex_labels{$key} = q|8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/p2pNetwork/;
+$external_latex_labels{$key} = q|9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/importTCP/;
+$external_latex_labels{$key} = q|6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/config/;
+$external_latex_labels{$key} = q|7|; 
+$noresave{$key} = "$nosave";
+
+1;
+
