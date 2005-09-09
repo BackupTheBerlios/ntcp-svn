c2a903e740f2e52f4301f3a758ea778d3002d28d
lgaballo <lgaballo@ae816e9b-97f9-0310-aa70-bf55b011afa0>
2005-09-09 08:46:26 +0000

    Added Client & Server factory

--- branches/beta/ntcp/test/TcpFactory.py
+++ branches/beta/ntcp/test/TcpFactory.py
@@ -2,42 +2,41 @@ from twisted.internet.protocol import Protocol, ClientFactory, DatagramProtocol
 from twisted.internet import reactor
 from sys import stdout, stdin
 
-
-class Chat:
-    def readWrite(self, transport, simulator):
-                  
-        print ' ',simulator.uri,'>> ',
-        data=stdin.readline()
-        while data != 'exit\n':
-            transport.write(data)
-            print simulator.uri,'>> ',
-            data=stdin.readline()
-        transport.loseConnection() 
-      
 class TcpConnection(Protocol):
     """
     All the Twisted functions for a TCP connection (c/s) 
     """
-    def __init__(self, reactor, simulator):
-        self.reactor = reactor
-        self.simulator = simulator 
         
     def dataReceived(self, data):
-        print self.simulator.remote,'>>',data
+        print '>>',data
 
     def connectionMade(self):
         print 'Connection Made...now you can send data...'
 
-        print ' ',self.simulator.uri,'>> ',
+        print '>> ',
         data=stdin.readline()
         while data != 'exit\n':
             self.transport.write(data)
-            print self.simulator.uri,'>> ',
+            print '>> ',
             data=stdin.readline()
         self.transport.loseConnection()
+
+    def connectionLost(self, reason):
+        print 'Lost connection.  Reason:', reason
+
+    def startedConnecting(self, connector):
+        pass
+      
+class TcpServer(Protocol):
+    """
+    All the Twisted functions for a TCP connection (c/s) 
+    """
         
-##         chat = Chat()
-##         self.reactor.callInThread(chat.readWrite, self.transport, self.simulator)
+    def dataReceived(self, data):
+        print '>>',data
+
+    def connectionMade(self):
+        print 'Connection Made...now you can receive data...'
 
     def connectionLost(self, reason):
         print 'Lost connection.  Reason:', reason
@@ -45,18 +44,23 @@ class TcpConnection(Protocol):
     def startedConnecting(self, connector):
         pass
     
+    def buildProtocol(self, addr):
+        return TcpServer()
+        
+    def clientConnectionLost(self, connector, reason):
+        print 'Lost connection.  Reason:', reason
+        
+    def clientConnectionFailed(self, connector, reason):
+        print 'Connection failed. Reason:', reason
+    
+      
 class TcpFactory(ClientFactory):
     
-    def __init__(self, reactor, simulator):
-        self.reactor = reactor
-        self.simulator = simulator 
-        
-        
     def startedConnecting(self, connector):
         pass
     
     def buildProtocol(self, addr):
-        return TcpConnection(self.reactor, self.simulator)
+        return TcpConnection()
     
     def clientConnectionLost(self, connector, reason):
         print 'Lost connection.  Reason:', reason
